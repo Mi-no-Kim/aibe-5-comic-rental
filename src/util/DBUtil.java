0664cbf174e259d43229f80fed1b9c2bae74a2d1
Mi-no-Kim <71382553+Mi-no-Kim@users.noreply.github.com>
2026-03-11 21:58:34 +0900

    refactor: DataSource 사용 방식으로 변경

--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -1,18 +1,28 @@
 package util;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import static util.ConnectionConst.*;
 
-
 public class DBUtil {
+
+    private static final DataSource dataSource;
+
+    static {
+        MysqlDataSource ds = new MysqlDataSource();
+        ds.setURL(URL);
+        ds.setUser(USERNAME);
+        ds.setPassword(PASSWORD);
+        dataSource = ds;
+    }
 
     public static Connection getConnection() {
         try {
-            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            return conn;
+            return dataSource.getConnection();
         } catch (SQLException e) {
             throw new IllegalStateException(e);
         }

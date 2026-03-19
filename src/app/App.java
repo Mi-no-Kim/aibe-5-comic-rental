31de6275b81d65b2a9e1729ad787ffe76e08a60c
unknown <sjwwiies2829@gmail.com>
2026-03-19 18:12:50 +0900

    merge conflict 해결

--- src/app/App.java
+++ src/app/App.java
@@ -1,10 +1,10 @@
 package app;
 
 import contoller.ComicController;
+import contoller.MemberController;
 import contoller.RentalController;
 import util.DateHolder;
 import util.Rq;
-
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.function.Consumer;
@@ -14,6 +14,7 @@ public class App {
 	private final Scanner sc;
 	private final ComicController comicController;
 	private final RentalController rentalController;
+	private final MemberController memberController;
 	private final DateHolder dateHolder = new DateHolder();
 
 	private final HashMap<String, Consumer<Rq>> commandMap = new HashMap<>();
@@ -24,6 +25,7 @@ public class App {
 		this.sc = new Scanner(System.in);
 		this.comicController = new ComicController();
 		this.rentalController = new RentalController();
+		this.memberController = new MemberController();
 		this.rentalController.setDateHolder(this.dateHolder);
 
 		commandMap.put("comic-add", comicController::comicAdd);
@@ -32,6 +34,28 @@ public class App {
 		commandMap.put("comic-search", comicController::comicSearch);
 		commandMap.put("comic-update", comicController::comicUpdate);
 		commandMap.put("comic-delete", comicController::comicDelete);
+		
+		
+		// member
+		commandMap.put("member-add", rq -> memberController.addMember());
+		commandMap.put("member-list", rq -> memberController.showMembers());
+		commandMap.put("member-search", rq -> {
+		    String keyword = rq.getParams().get(0);
+		    memberController.searchMember(keyword);
+		});
+		commandMap.put("member-delete", rq -> {
+		    int id = Integer.parseInt(rq.getParams().get(0));
+		    memberController.deleteMember(id);
+		});
+		commandMap.put("member-update", rq -> {
+		    if (rq.getParams().size() == 0) {
+		        System.out.println("회원 id를 입력해주세요.");
+		        return;
+		    }
+
+		    int id = Integer.parseInt(rq.getParams().get(0));
+		    memberController.updateMember(id);
+		});
 
 		commandMap.put("exit", this::exit);
 	}
@@ -60,9 +84,14 @@ public class App {
 				if (!runState) break;
 			} catch (Exception e) {
 				System.out.println("=> 명령어 형식이 올바르지 않습니다.");
+
+				continue;
+			}
+			
+
+			
 			}
 		}
-	}
 
 	private void exit(Rq rq) {
 		runState = false;
--- src/util/ConnectionConstForm.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public class ConnectionConstForm {
-    public static final String URL = "jdbc:mysql://localhost:portNumber/DataBaseName";
-    public static final String USERNAME = "userName";
-    public static final String PASSWORD = "password";
-}
